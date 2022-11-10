5203bb63cad30b17e223c1ccfd8c532a8d7527fc
artless <jayanlas00@gmail.com>
2022-11-10 13:09:58 -0500

    added a whole of shit

--- Candyland Final/Form1.Designer.cs
+++ Candyland Final/Form1.Designer.cs
@@ -29,14 +29,19 @@ namespace Candyland_Final
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.boardImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boardImage)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(144, 127);
+            this.label1.Location = new System.Drawing.Point(12, 524);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
@@ -44,23 +49,46 @@ namespace Candyland_Final
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(137, 205);
+            this.button1.Location = new System.Drawing.Point(389, 518);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(99, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Draw a card";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(120, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(15, 15);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // boardImage
+            // 
+            this.boardImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.boardImage.Image = ((System.Drawing.Image)(resources.GetObject("boardImage.Image")));
+            this.boardImage.Location = new System.Drawing.Point(0, 0);
+            this.boardImage.Name = "boardImage";
+            this.boardImage.Size = new System.Drawing.Size(500, 500);
+            this.boardImage.TabIndex = 3;
+            this.boardImage.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(500, 553);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.boardImage);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boardImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +98,8 @@ namespace Candyland_Final
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox boardImage;
     }
 }
 
--- Candyland Final/Form1.cs
+++ Candyland Final/Form1.cs
@@ -28,6 +28,23 @@ namespace Candyland_Final
             string actualSquare = squareTypeSet[num].ToString();
 
             label1.Text = "The Square to move to is: " + actualSquare;
+
+            if(pictureBox1.Location.X < 450)
+            {
+                pictureBox1.Left += 20;
+            }
+            if(pictureBox1.Location.X >= 450)
+            {
+                MessageBox.Show("x is above 450: " + pictureBox1.Location.X);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            boardImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Load("https://img.pokemondb.net/artwork/bulbasaur.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
         }
     }
 }
