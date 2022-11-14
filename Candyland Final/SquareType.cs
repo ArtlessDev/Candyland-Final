b42e317f2e00ddc70c424945a40413c569c575c7
Jisan Jung <jjjung13@gmail.com>
2022-11-14 18:29:36 -0500

    implement square type class

---
+++ Candyland Final/SquareType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candyland_Final
+{
+    class SquareType
+    {
+        private string[] colorTypes = { 
+                                   "doubleBlue", 
+                                   "doubleGreen", 
+                                   "doubleOrange",
+                                   "doublePurple",
+                                   "doubleRed",
+                                   "doubleYellow",
+                                   "singleBlue",
+                                   "singleGreen",
+                                   "singleOrange",
+                                   "singlePurple",
+                                   "singleRed",
+                                   "singleYellow",
+                                   "special"
+                                  };
+        private string[] specialTypes = {
+                                    "specialCandycane",
+                                    "specialDonut",
+                                    "specialGeneric",
+                                    "specialIcecream",
+                                    "specialLollipop"
+                                    };
+
+        public string[] ColorTypes
+        {
+            get { return this.colorTypes; }
+        }
+        public string[] SpecialTypes
+        {
+            get { return this.specialTypes; }
+        }
+    }
+}
